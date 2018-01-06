29d567c9541ea9b54eeebd88196fa02b5210ea5b
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2018-01-06 16:20:19 +0900

    第2章

---
+++ ch02/Random/Random/RandomController.swift
@@ -0,0 +1,19 @@
+//
+//  RandomController.swift
+//  Random
+//
+//  Created by 布川祐人 on 2018/01/06.
+//  Copyright © 2018年 NUNOKAWA Masato. All rights reserved.
+//
+
+import Cocoa
+
+class RandomController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
