6133a0942b08de9a23efa935a212e7672723cebc
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2018-01-06 22:10:57 +0900

    6章 課題2 TodoChallengeの実装

--- ch06/TodoChallenge/TodoChallenge/AppDelegate.swift
+++ ch06/TodoChallenge/TodoChallenge/AppDelegate.swift
@@ -9,11 +9,14 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource {
 
     @IBOutlet weak var window: NSWindow!
-
-
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var textField: NSTextField!
+    
+    var lines : [String] = []
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
@@ -22,6 +25,30 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    @IBAction func addButtonClicked(_ sender: Any) {
+        lines.append(textField.stringValue)
+        tableView.reloadData()
+    }
+    
+    // MARK :- NSTableViewDataSource
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return lines.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        return lines[row]
+    }
+    
+    func tableView(_ tableView: NSTableView, setObjectValue object: Any?, for tableColumn: NSTableColumn?, row: Int) {
+        //NSLog("update %d", row)
+        let newValue = object as! String
 
+        if newValue.count == 0 {
+            lines.remove(at: row)
+        } else {
+            lines[row] = newValue
+        }
+        tableView.reloadData()
+    }
 }
 
