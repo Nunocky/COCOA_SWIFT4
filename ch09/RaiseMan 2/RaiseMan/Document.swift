ecd014c07beaefdafdfbadbaf96b8c466f5b987f
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2018-01-08 15:12:57 +0900

    9章 新しい従業員の追加と同時に編集を開始する

---
+++ ch09/RaiseMan 2/RaiseMan/Document.swift
@@ -0,0 +1,180 @@
+//
+//  Document.swift
+//  RaiseMan
+//
+//  Created by 布川祐人 on 2018/01/07.
+//  Copyright © 2018年 NUNOKAWA Masato. All rights reserved.
+//
+
+import Cocoa
+
+private var kViewControllerContext: UInt8 = 0
+
+class Document: NSDocument {
+    
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet var employeeController: NSArrayController!
+
+    @objc
+    var employees : [Person] {
+        willSet {
+            for p:Person in employees {
+                self.stopObservingPerson(p)
+            }
+        }
+        didSet {
+            for p:Person in employees {
+                self.startObservingPerson(p)
+            }
+        }
+    }
+    
+    override init() {
+        employees = []
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override var windowNibName: NSNib.Name? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return NSNib.Name("Document")
+    }
+
+    override func data(ofType typeName: String) throws -> Data {
+        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
+        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
+        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+    }
+
+    override func read(from data: Data, ofType typeName: String) throws {
+        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
+        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
+        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
+        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+    }
+
+    @objc dynamic
+    func insertObject(_ employee : Person, inEmployeesAtIndex index: Int) {
+        let undo = self.undoManager!
+        
+        undo.registerUndo(withTarget: self) { targetSelf in
+            targetSelf.removeObjectFromEmployeesAtIndex(index)
+        }
+        
+        if !undo.isUndoing {
+            undo.setActionName("Add Person")
+        }
+
+        startObservingPerson(employee)
+        employees.insert(employee, at: index)
+    }
+    
+    @objc dynamic
+    func removeObjectFromEmployeesAtIndex(_ index: Int) {
+        let person : Person = employees[index]
+        let undo = self.undoManager!
+        
+        undo.registerUndo(withTarget: self) { targetSelf in
+            targetSelf.insertObject(person, inEmployeesAtIndex: index)
+        }
+        
+        if !undo.isUndoing {
+            undo.setActionName("Remove Person")
+        }
+        
+        stopObservingPerson(person)
+        employees.remove(at: index)
+    }
+    
+    func startObservingPerson(_ p : Person) {
+        p.addObserver(self,
+                      forKeyPath: "personName",
+                      options: NSKeyValueObservingOptions.old,
+                      context: &kViewControllerContext)
+
+        p.addObserver(self,
+                      forKeyPath: "expectedRaise",
+                      options: NSKeyValueObservingOptions.old,
+                      context: &kViewControllerContext)
+    }
+
+    func stopObservingPerson(_ p : Person) {
+        p.removeObserver(self, forKeyPath: "personName")
+        p.removeObserver(self, forKeyPath: "expectedRaise")
+    }
+    
+    func changeKeyPath(keyPath:String,
+                       ofObject obj:AnyObject,
+                       toValue newValue:AnyObject) {
+        obj.setValue(newValue, forKeyPath:keyPath)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if context != &kViewControllerContext {
+            // コンテキストが一致しない場合、このメッセージは
+            // スーパークラスに宛てたものと判断される
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            return
+        }
+        
+        guard let undo = self.undoManager else {
+            return
+        }
+        
+        let oldValue = change?[.oldKey]
+        
+        undo.registerUndo(withTarget: self) { (targetSelf) in
+            targetSelf.changeKeyPath(keyPath:keyPath!,
+                                     ofObject:object as AnyObject,
+                                     toValue:oldValue as AnyObject)
+        }
+        
+        undo.setActionName("Edit")
+    }
+    
+    @IBAction func createEmployee(_ sender : Any) {
+        guard let w = tableView.window else {
+            return
+        }
+        
+        let editingEnded = w.makeFirstResponder(w)
+        if !editingEnded {
+            NSLog("Unable to end editing")
+            return
+        }
+        
+        guard let undo = self.undoManager else {
+            return
+        }
+        
+        // このイベントの中ですでに編集が発生しているか
+        if undo.groupingLevel > 0 {
+            undo.endUndoGrouping()
+            undo.beginUndoGrouping()
+        }
+        
+        // オブジェクトを作成する
+        let p = employeeController.newObject() as! Person
+        
+        // employeeControllerのコンテンツ配列に追加する
+        employeeController.addObject(p)
+        
+        // 再度並べ替える (ユーザー列の並べ替えをした際に備えて)
+        employeeController.rearrangeObjects()
+        
+        // 並べ替えられた配列を取得する
+        let a = employeeController.arrangedObjects as! [Person]
+        
+        // 追加されたオブジェクトを検索する
+        if let row = a.index(of: p) {
+            NSLog("starting edit of %@ in row %lu", p, row)
+            tableView.editColumn(0, row: row, with: nil, select: true)
+        }
+    }
+}
+
