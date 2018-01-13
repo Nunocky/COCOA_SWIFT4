98c3ce84657b9d6bc5278e3c29884d845d780215
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2018-01-13 15:22:33 +0900

    11章 initial commit

---
+++ ch11/CarLot/CarLot/Document.swift
@@ -0,0 +1,28 @@
+//
+//  Document.swift
+//  CarLot
+//
+//  Created by 布川祐人 on 2018/01/13.
+//  Copyright © 2018年 NUNOKAWA Masato. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override var windowNibName: NSNib.Name? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return NSNib.Name("Document")
+    }
+
+}
