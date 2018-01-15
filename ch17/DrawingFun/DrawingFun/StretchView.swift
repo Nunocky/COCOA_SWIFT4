3fe3490917b4b44d48918f54669614805f95a70d
Masato NUNOKAWA <masato.nunokawa@gmail.com>
2018-01-15 19:54:24 +0900

    17章 StretchViewの追加

---
+++ ch17/DrawingFun/DrawingFun/StretchView.swift
@@ -0,0 +1,19 @@
+//
+//  StretchView.swift
+//  DrawingFun
+//
+//  Created by 布川祐人 on 2018/01/15.
+//  Copyright © 2018年 NUNOKAWA Masato. All rights reserved.
+//
+
+import Cocoa
+
+class StretchView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
