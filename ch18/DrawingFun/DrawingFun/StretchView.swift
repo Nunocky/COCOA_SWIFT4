863b3c2953d26a5fa670df50da5490b5218123d2
Masato NUNOKAWA <masato.nunokawa@gmail.com>
2018-01-15 20:18:30 +0900

    18章 マウスイベントの取得

---
+++ ch18/DrawingFun/DrawingFun/StretchView.swift
@@ -0,0 +1,79 @@
+//
+//  StretchView.swift
+//  DrawingFun
+//
+//  Created by 布川祐人 on 2018/01/15.
+//  Copyright © 2018年 NUNOKAWA Masato. All rights reserved.
+//
+
+import Cocoa
+
+class StretchView: NSView {
+
+    var path : NSBezierPath
+    
+//    override init(frame frameRect: NSRect) {
+//        super.init(frame: frameRect)
+//    }
+    
+    required init?(coder decoder: NSCoder) {
+        //fatalError("init(coder:) has not been implemented")
+        path = NSBezierPath()
+        
+        super.init(coder: decoder)
+
+        path.lineWidth = 3.0
+        var p = randomPoint()
+        path.move(to: p)
+        for _ in 0 ..< 15 {
+            p = randomPoint()
+            path.line(to: p)
+        }
+        
+        path.close()
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+        NSColor.green.set()
+        NSBezierPath.fill(self.bounds)
+        
+        NSColor.white.set()
+        path.stroke()
+        //path.fill()
+    }
+
+    func randomPoint() -> NSPoint {
+        var result = NSPoint()
+        let r = self.bounds
+        
+        result.x = r.origin.x + (CGFloat)(arc4random() % (UInt32)(r.size.width))
+        result.y = r.origin.y + (CGFloat)(arc4random() % (UInt32)(r.size.height))
+        return result
+    }
+    
+    // ビューを反転系で扱う
+    override var isFlipped: Bool {
+        get {
+            return true
+        }
+    }
+    
+    // MARK: - Events
+    override func mouseDown(with event: NSEvent) {
+        NSLog("mouseDown: %ld", event.clickCount)
+    }
+    
+    override func mouseDragged(with event: NSEvent) {
+        let p = event.locationInWindow
+        NSLog("mouseDragged: %@", NSStringFromPoint(p))
+    }
+    
+    
+    override func mouseUp(with event: NSEvent) {
+        NSLog("mouseUp")
+    }
+}
+
